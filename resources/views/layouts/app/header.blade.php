4c503fbb5ada495dce6f3b5a8f589aed5b509028
josepueblaphcs <jose.puebla@phcs.cl>
2026-01-25 21:56:08 -0300

    Inicio App

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -2,7 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         @include('partials.head')
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
